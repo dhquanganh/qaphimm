e695cccbdd169a9bc46714fa8dbab90577087d6c
dqa <quanganh2004dasuo@gmail.com>
2026-03-09 12:11:04 +0700

    fixing

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homeController');
 
-router.get("/movie/:id", homeController.movieShow);
+router.get("/movie", homeController.movieShow);
 router.get("/search", homeController.movieSearch);
 router.get("/movie-list", homeController.movieList);
 router.get("/tv-series", homeController.tvSeries);
